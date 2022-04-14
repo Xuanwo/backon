a563165f1d87bdff617e741eb0449252926a231a
Xuanwo <github@xuanwo.io>
2022-04-14 15:15:28 +0800

    Add docs
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@
 //!
 //! # Examples
 //!
+//! Retry will default settings.
+//!
 //! ```no_run
 //! use backon::Retryable;
 //! use backon::ExponentialBackoff;
@@ -27,8 +29,30 @@
 //! #[tokio::main]
 //! async fn main() -> Result<()> {
 //!     let content = fetch.retry(ExponentialBackoff::default()).await?;
-//!     println!("fetch succeeded: {}", content);
 //!
+//!     println!("fetch succeeded: {}", content);
+//!     Ok(())
+//! }
+//! ```
+//!
+//! Retry will specify retryable error.
+//!
+//! ```no_run
+//! use backon::Retryable;
+//! use backon::ExponentialBackoff;
+//! use anyhow::Result;
+//!
+//! async fn fetch() -> Result<String> {
+//!     Ok(reqwest::get("https://www.rust-lang.org").await?.text().await?)
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<()> {
+//!     let content = fetch
+//!         .retry(ExponentialBackoff::default())
+//!         .with_error_fn(|e| e.to_string() == "retryable").await?;
+//!
+//!     println!("fetch succeeded: {}", content);
 //!     Ok(())
 //! }
 //! ```
