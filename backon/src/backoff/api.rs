1c34c4ab28134595a7f10f1e24577af757612a38
Xuanwo <github@xuanwo.io>
2024-09-04 19:47:30 +0800

    refactor: Remove the Debug requirement of Backoff (#141)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- backon/src/backoff/api.rs
+++ backon/src/backoff/api.rs
@@ -1,8 +1,7 @@
-use core::fmt::Debug;
 use core::time::Duration;
 
 /// BackoffBuilder is utilized to construct a new backoff.
-pub trait BackoffBuilder: Debug + Send + Sync + Unpin {
+pub trait BackoffBuilder: Send + Sync + Unpin {
     /// The associated backoff returned by this builder.
     type Backoff: Backoff;
 
@@ -15,4 +14,4 @@ pub trait BackoffBuilder: Debug + Send + Sync + Unpin {
 /// - `Some(Duration)` indicates the caller should `sleep(Duration)` and retry the request.
 /// - `None` indicates the limits have been reached, and the caller should return the current error instead.
 pub trait Backoff: Iterator<Item = Duration> + Send + Sync + Unpin {}
-impl<T> Backoff for T where T: Iterator<Item = Duration> + Debug + Send + Sync + Unpin {}
+impl<T> Backoff for T where T: Iterator<Item = Duration> + Send + Sync + Unpin {}
