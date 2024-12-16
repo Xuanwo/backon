41decb170bc5ded589dbb98cc995519bce848999
Alexander Walter <alexander@wackazong.com>
2024-12-16 16:41:38 +0100

    Use Rng in std and no_std

--- backon/src/backoff/mod.rs
+++ backon/src/backoff/mod.rs
@@ -12,3 +12,5 @@ pub use fibonacci::FibonacciBuilder;
 mod exponential;
 pub use exponential::ExponentialBackoff;
 pub use exponential::ExponentialBuilder;
+
+const RANDOM_SEED: u64 = 0x6261636b6f6e;
